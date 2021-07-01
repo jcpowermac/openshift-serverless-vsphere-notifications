52b8afca067750a9c77216134d1467e7d37fef3c
Joseph Callen <jcallen@redhat.com>
2021-07-01 18:10:10 -0400

    change tostring percentage;cron updates

--- notification.ps1
+++ notification.ps1
@@ -5,13 +5,13 @@ Connect-VIServer -Server $Env:VCENTER_URI -Credential (Import-Clixml $Env:VCENTE
 $hosts = Get-VMHost
 
 foreach($item in $hosts) {
-    $percentage = [int](($item.CpuUsageMhz / $item.CpuTotalMhz)*100)
+    $percentage = ($item.CpuUsageMhz / $item.CpuTotalMhz).tostring("P")
 
     if($percentage -ge 90) {
-        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ":fire: Host: $($item.Name), CPU: $($percentage)"
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ":fire: Host: $($item.Name), CPU: $($percentage)%"
     }
     elseif( $percentage -ge 80) {
-        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text "Host: $($item.Name), CPU: $($percentage)"
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ":mag: Host: $($item.Name), CPU: $($percentage)%"
     }
     else {
         Write-Host "Host is ok"
