35dbe582fa3f132efe7a4d5633800a09451b8016
Joseph Callen <jcallen@redhat.com>
2021-07-01 16:09:19 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # openshift-serverless-vsphere-notifications
+
+
+### slack webhook uri
+
+```
+oc create secret generic slack-webhook-uri --from-literal=uri=
+```
+
+# TODO: How to create creds.xml
+```
+  oc create secret generic vcenter-credential --from-file=creds.xml=creds.xml
+
+```
---
+++ notification.ps1
@@ -0,0 +1,20 @@
+#!/bin/pwsh
+
+Connect-VIServer -Server $Env:VCENTER_URI -Credential (Import-Clixml $Env:VCENTER_SECRET_PATH)
+
+
+$hosts = Get-VMHost
+
+foreach($item in $hosts) {
+    $percentage = [int](($item.CpuUsageMhz / $item.CpuTotalMhz)*100)
+
+    if($percentage -ge 90) {
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ":fire: Host: $($item.Name), CPU: $($percentage)"
+    }
+    elseif( $percentage -ge 80) {
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text "Host: $($item.Name), CPU: $($percentage)"
+    }
+    else {
+        Write-Host "Host is ok"
+    }
+}
