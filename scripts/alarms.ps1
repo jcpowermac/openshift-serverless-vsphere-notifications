76bea628642b584478a2988822f9b51a5fabe3a8
Joseph Callen <jcallen@redhat.com>
2021-10-19 14:29:14 -0400

    add yellow alarms, run only every 8 hours

--- scripts/alarms.ps1
+++ scripts/alarms.ps1
@@ -8,9 +8,16 @@ $AlarmStates = (Get-Datacenter).ExtensionData.TriggeredAlarmState
 
 foreach ($as in $AlarmStates) {
     $alarm = Get-View -Id $as.Alarm
+    $entity = Get-View -Id $as.Entity
 
     if($as.OverallStatus -eq "red"){
-        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text " :fire: $($alarm.Info.Name)"
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ":fire: $($entity.Name): $($alarm.Info.Name)"
+    }
+    # To lower the amount of alerts run only every 8 hours
+    if(((get-date -Format HH) % 8) -eq 0) {
+        if($as.OverallStatus -eq "yellow"){
+            Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text "$($entity.Name): $($alarm.Info.Name)"
+        }
     }
 }
 
