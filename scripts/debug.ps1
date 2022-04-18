a3eb0298b8876153016e51027a796a681f146fd4
Joseph Callen <jcallen@redhat.com>
2022-04-18 15:46:25 -0400

    additional debug dhcp issues

---
+++ scripts/debug.ps1
@@ -0,0 +1,37 @@
+#!/bin/pwsh
+
+try {
+
+    Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
+
+    Connect-VIServer -Server $Env:VCENTER_URI -Credential (Import-Clixml $Env:VCENTER_SECRET_PATH) | Out-Null
+
+    # CI has moved virtual machines to the debug folder we better take a look
+    #
+    #
+    $debugFolder = get-folder -Name Debug
+
+    if ($?) {
+       Get-VM -Location $debugFolder | %{
+           Get-ViEvents -Entity $_ >> "/var/log/debug/$($_.Name).txt"
+       }
+
+
+
+
+
+       # if($debugVirtualMachines.Count -gt 0) {
+       #     $debugVMSlackMsg = ":fire: VMs in debug folder: $($debugVirtualMachines -join ',')"
+       #     Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text $debugVMSlackMsg
+       # }
+    }
+}
+catch {
+    Get-Error
+    exit 1
+}
+finally {
+    Disconnect-VIServer -Server * -Force:$true -Confirm:$false
+}
+
+exit 0
