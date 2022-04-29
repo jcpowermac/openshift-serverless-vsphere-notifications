fea7f6227d522edb50adbf2cce9e5b52b375513c
Joseph Callen <jcallen@redhat.com>
2022-04-29 10:42:53 -0400

    adding testing script

--- scripts/variables-testing.ps1
+++ scripts/variables-testing.ps1
@@ -1,23 +1,25 @@
 #!/bin/pwsh
-. .\variables.ps1
+. /var/run/config/vcenter/variables.ps1
 
-try {
-
-    $cihash = ConvertFrom-Json -InputObject $ci -AsHashtable
-    foreach ($key in $cihash.Keys) {
-        $cihash[$key].vcenter
-
-#    Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
-#    Connect-VIServer -Server $Env:VCENTER_URI -Credential (Import-Clixml $Env:VCENTER_SECRET_PATH) | Out-Null
+Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
+$cihash = ConvertFrom-Json -InputObject $ci -AsHashtable
+foreach ($key in $cihash.Keys) {
+    $cihash[$key].vcenter
+    try {
+        Connect-VIServer -Server $Env:VCENTER_URI -Credential (Import-Clixml $cihash[$key].secret) | Out-Null
+        Get-VM
+        Get-Datastore -Name $cihash[$key].datastore
+        Get-Datacenter -Name $cihash[$key].datacenter
+        Get-Cluster -Name $cihash[$key].datacenter
     }
-}
-catch {
-    Get-Error
-    exit 1
-}
-finally {
-    #Disconnect-CisServer -Server * -Force:$true -Confirm:$false
-    #Disconnect-VIServer -Server * -Force:$true -Confirm:$false
+    catch {
+        Get-Error
+        exit 1
+    }
+    finally {
+        #Disconnect-CisServer -Server * -Force:$true -Confirm:$false
+        Disconnect-VIServer -Server * -Force:$true -Confirm:$false
+    }
 }
 
 exit 0
