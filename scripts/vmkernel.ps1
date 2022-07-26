0acee27cfd6036df3f10e404f504345ed55a3e10
Joseph Callen <jcallen@redhat.com>
2022-07-26 16:02:56 -0400

    watch for nfs connection drops

---
+++ scripts/vmkernel.ps1
@@ -0,0 +1,60 @@
+#!/bin/pwsh
+. /var/run/config/vcenter/variables.ps1
+
+Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
+$cihash = ConvertFrom-Json -InputObject $ci -AsHashtable
+$slackMessage = @"
+vcenter: {0}
+"@
+
+$slackMessage = @"
+vmkernel
+vcenter: {0}
+SideIndicator: {1}
+Messages: {2}
+"@
+
+#$messages = @('lost connection','nfs','failure')
+$messages = @('lost connection')
+
+if (-not(Test-Path -Path "/var/run/secret/logs/old.log" -PathType Leaf)) {
+    $null = New-Item -ItemType File -Path "/var/run/secret/logs/old.log" -Force 
+}
+
+foreach ($key in $cihash.Keys) {
+    try {
+        $cihash[$key].vcenter
+        $cihash[$key].datacenter
+        $cihash[$key].cluster
+        $cihash[$key].datastore
+
+        Connect-VIServer -Server $cihash[$key].vcenter -Credential (Import-Clixml $cihash[$key].secret) | Out-Null
+
+        $hosts = Get-VMHost
+
+        $hosts | ForEach-Object {
+
+            $_.Name
+
+            $entries = ($_ | Get-Log -Key "vmkernel" -StartLineNum 1 -NumLines 10000 ).Entries
+
+            $messages | ForEach-Object {
+                $entries -imatch $_ | Out-File -Append -FilePath "/var/run/secret/logs/new.log"
+            }
+            $compare = Compare-Object -ReferenceObject (Get-Content "/var/run/secret/logs/old.log") -DifferenceObject (Get-Content "/var/run/secret/logs/new.log")
+
+            if ($compare -ne $null) {
+                Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $cihash[$key].vcenter, $compare.SideIndicator, $compare.InputObject)
+            }
+        }
+    }
+    catch {
+        Get-Error
+    }
+    finally {
+        Move-Item -Force:$true -Confirm:$false -Path "/var/run/secret/logs/new.log" -Destination "/var/run/secret/logs/old.log"
+        Disconnect-VIServer -Server * -Force:$true -Confirm:$false
+    }
+}
+
+exit 0
