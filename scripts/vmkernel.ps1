9d2774f7d3eefb8d677414e7b6bf53d8a1c60b4c
Joseph Callen <jcallen@redhat.com>
2022-07-27 10:24:27 -0400

    move to deployment for vmkernel

--- scripts/vmkernel.ps1
+++ scripts/vmkernel.ps1
@@ -3,9 +3,6 @@
 
 Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
 $cihash = ConvertFrom-Json -InputObject $ci -AsHashtable
-$slackMessage = @"
-vcenter: {0}
-"@
 
 $slackMessage = @"
 vmkernel
@@ -14,50 +11,54 @@ SideIndicator: {1}
 Messages: {2}
 "@
 
+$newFile = "/projects/new.log"
+$oldFile = "/projects/old.log"
+
 #$messages = @('lost connection','nfs','failure')
 $messages = @('lost connection')
 
-#if (-not(Test-Path -Path "/var/run/secret/logs/old.log" -PathType Leaf)) {
-#    $null = New-Item -ItemType File -Path "/var/run/secret/logs/old.log" -Force 
-#}
+if (-not(Test-Path -Path $oldFile -PathType Leaf)) {
+    $null = New-Item -ItemType File -Path $oldFile -Force
+}
 
-ls -alh /var/run/secret/logs/
 
-foreach ($key in $cihash.Keys) {
-    try {
-        $cihash[$key].vcenter
-        $cihash[$key].datacenter
-        $cihash[$key].cluster
-        $cihash[$key].datastore
+while ($true) {
+    foreach ($key in $cihash.Keys) {
+        try {
+            $cihash[$key].vcenter
+            $cihash[$key].datacenter
+            $cihash[$key].cluster
+            $cihash[$key].datastore
 
-        Connect-VIServer -Server $cihash[$key].vcenter -Credential (Import-Clixml $cihash[$key].secret) | Out-Null
+            Connect-VIServer -Server $cihash[$key].vcenter -Credential (Import-Clixml $cihash[$key].secret) | Out-Null
 
-        $hosts = Get-VMHost
+            $hosts = Get-VMHost
 
-        $hosts | ForEach-Object {
+            $hosts | ForEach-Object {
+                $entries = ($_ | Get-Log -Key "vmkernel" -StartLineNum 1 -NumLines 10000 ).Entries
 
-            $_.Name
+                $messages | ForEach-Object {
+                    $entries -imatch $_ | Out-File -Append -FilePath $newFile
+                }
 
-            $entries = ($_ | Get-Log -Key "vmkernel" -StartLineNum 1 -NumLines 10000 ).Entries
-
-            $messages | ForEach-Object {
-                $entries -imatch $_ | Out-File -Append -FilePath "/var/run/secret/logs/new.log"
             }
-            $compare = Compare-Object -ReferenceObject (Get-Content "/var/run/secret/logs/old.log") -DifferenceObject (Get-Content "/var/run/secret/logs/new.log")
-
-            if ($compare -ne $null) {
+            $compare = Compare-Object -ReferenceObject (Get-Content $oldFile) -DifferenceObject (Get-Content $newFile)
+            if ($null -ne $compare) {
                 Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $cihash[$key].vcenter, $compare.SideIndicator, $compare.InputObject)
             }
         }
+        catch {
+            Get-Error
+        }
+        finally {
+            Get-Content -Path $newFile -Raw | Set-Content -Path $oldFile
+            #Move-Item -Force:$true -Confirm:$false -Path "/var/run/secret/logs/new.log" -Destination "/var/run/secret/logs/old.log"
+            Disconnect-VIServer -Server * -Force:$true -Confirm:$false
+        }
     }
-    catch {
-        Get-Error
-    }
-    finally {
-        Get-Content -Path "/var/run/secret/logs/new.log" -Raw | Set-Content -Path "/var/run/secret/logs/old.log"
-        #Move-Item -Force:$true -Confirm:$false -Path "/var/run/secret/logs/new.log" -Destination "/var/run/secret/logs/old.log"
-        Disconnect-VIServer -Server * -Force:$true -Confirm:$false
-    }
-}
 
-exit 0
+
+    Write-Host "Starting to sleep..."
+
+    Start-Sleep -Seconds 3600 
+}
