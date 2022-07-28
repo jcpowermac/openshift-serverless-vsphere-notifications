325ebd6ccce83cabe6f72e68b79955746a90291e
Joseph Callen <jcallen@redhat.com>
2022-07-28 16:07:04 -0400

    cns vol fixes

--- scripts/cnsvols.ps1
+++ scripts/cnsvols.ps1
@@ -2,32 +2,49 @@
 
 . /var/run/config/vcenter/variables.ps1
 
+Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
 $cihash = ConvertFrom-Json -InputObject $ci -AsHashtable
+
+$slackMessage = @"
+cns volumes
+vcenter: {0}
+count: {1}
+"@
 
 foreach ($key in $cihash.Keys) {
 
     $Env:GOVC_URL = $cihash[$key].vcenter
+    $Env:GOVC_DATACENTER = $cihash[$key].datacenter
     $Env:GOVC_INSECURE = 1
 
     try {
-        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text "Cleaning: $($cihash[$key].vcenter)"
+        #Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text "Cleaning: $($cihash[$key].vcenter)"
 
-        Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
-        Connect-VIServer -Server $cihash[$key].vcenter -Credential (Import-Clixml $cihash[$key].secret) | Out-Null
+        $credential = Import-Clixml -Path $cihash[$key].secret
+
+        $Env:GOVC_USERNAME = [System.Net.NetworkCredential]::new("", $credential).UserName
+        $Env:GOVC_PASSWORD = [System.Net.NetworkCredential]::new("", $credential).Password
+
+        Connect-VIServer -Server $cihash[$key].vcenter -Credential $credential | Out-Null
 
 
-        $storagePolicies = (Get-SpbmStoragePolicy | Where-Object -Property Name -Like "*ci*" | Where-Object -Property CreationTime -LT $deleteday )
+        #$storagePolicies = (Get-SpbmStoragePolicy | Where-Object -Property Name -Like "*ci*" | Where-Object -Property CreationTime -LT $deleteday )
 
-        foreach ($policy in $storagePolicies) {
-            Remove-SpbmStoragePolicy -StoragePolicy $policy -Confirm:$false
-        }
+        #foreach ($policy in $storagePolicies) {
+        #    Remove-SpbmStoragePolicy -StoragePolicy $policy -Confirm:$false
+        #}
 
         $govcOutput = "./volumes.json"
         $govcError = "./govcerror.txt"
         $process = Start-Process -Wait -RedirectStandardError $govcError -RedirectStandardOutput $govcOutput -FilePath /bin/govc -ArgumentList @("volume.ls", "-json", "-ds $($cihash[$key].datastore)") -PassThru
 
         if ($process.ExitCode -eq 0) {
+
+
             $volumeHash = (Get-Content -Path $govcOutput | ConvertFrom-Json -AsHashtable)
+
+            Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $cihash[$key].vcenter, $volumeHash.Keys.Count)
+
             foreach ($vol in $volumeHash["Volume"]) {
                 $volumeId = $vol["VolumeId"]["Id"]
                 $clusterId = $vol["Metadata"]["ContainerCluster"]["ClusterId"]
