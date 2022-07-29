ac90b87b3bf46a295ef7ac77e10d4cbbcd17001d
Joseph Callen <jcallen@redhat.com>
2022-07-29 10:06:53 -0400

    fix cns

--- scripts/cnsvols.ps1
+++ scripts/cnsvols.ps1
@@ -11,39 +11,28 @@ vcenter: {0}
 count: {1}
 "@
 
+
 foreach ($key in $cihash.Keys) {
-
-    $Env:GOVC_URL = $cihash[$key].vcenter
-    $Env:GOVC_DATACENTER = $cihash[$key].datacenter
-    $Env:GOVC_INSECURE = 1
-
     try {
-        #Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text "Cleaning: $($cihash[$key].vcenter)"
-
         $credential = Import-Clixml -Path $cihash[$key].secret
 
-        $Env:GOVC_USERNAME = [System.Net.NetworkCredential]::new("", $credential).UserName
-        $Env:GOVC_PASSWORD = [System.Net.NetworkCredential]::new("", $credential).Password
+        $Env:GOVC_PASSWORD = $credential.GetNetworkCredential().Password
+        $Env:GOVC_USERNAME = $credential.GetNetworkCredential().UserName
+        $Env:GOVC_URL = $cihash[$key].vcenter
+        $Env:GOVC_DATACENTER = $cihash[$key].datacenter
+        $Env:GOVC_INSECURE = 1
 
         Connect-VIServer -Server $cihash[$key].vcenter -Credential $credential | Out-Null
 
-
-        #$storagePolicies = (Get-SpbmStoragePolicy | Where-Object -Property Name -Like "*ci*" | Where-Object -Property CreationTime -LT $deleteday )
-
-        #foreach ($policy in $storagePolicies) {
-        #    Remove-SpbmStoragePolicy -StoragePolicy $policy -Confirm:$false
-        #}
-
         $govcOutput = "./volumes.json"
         $govcError = "./govcerror.txt"
+
         $process = Start-Process -Wait -RedirectStandardError $govcError -RedirectStandardOutput $govcOutput -FilePath /bin/govc -ArgumentList @("volume.ls", "-json", "-ds $($cihash[$key].datastore)") -PassThru
 
         if ($process.ExitCode -eq 0) {
-
-
             $volumeHash = (Get-Content -Path $govcOutput | ConvertFrom-Json -AsHashtable)
 
-            Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $cihash[$key].vcenter, $volumeHash.Keys.Count)
+            Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $cihash[$key].vcenter, $volumeHash.Volume.Count)
 
             foreach ($vol in $volumeHash["Volume"]) {
                 $volumeId = $vol["VolumeId"]["Id"]
@@ -51,10 +40,9 @@ foreach ($key in $cihash.Keys) {
                 $clusterInventory = Get-Inventory -Name $clusterId -ErrorAction Continue
 
                 if ($clusterInventory.Count -eq 0) {
+                    Write-Host "Delete: $($volumeId)"
+
                     Start-Process -Wait -FilePath /bin/govc -ArgumentList @("volume.rm", $volumeId)
-                }
-                else {
-                    Write-Output $clusterInventory
                 }
             }
         }
@@ -65,9 +53,7 @@ foreach ($key in $cihash.Keys) {
     catch {
         $caught = Get-Error
         $errStr = $caught.ToString()
-
         $caught
-
         Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text $errStr
         exit 1
     }
@@ -75,5 +61,4 @@ foreach ($key in $cihash.Keys) {
         Disconnect-VIServer -Server * -Force:$true -Confirm:$false
     }
 }
-
 exit 0
