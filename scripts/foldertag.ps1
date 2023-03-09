233753216468c02bef8a930ff6c2fab526137c7a
Joseph Callen <jcallen@redhat.com>
2023-03-09 07:44:40 -0500

    remove regex for folder tag name

--- scripts/foldertag.ps1
+++ scripts/foldertag.ps1
@@ -24,7 +24,8 @@ foreach ($key in $cihash.Keys) {
         #Write-Host "Get-TagAssignment is slow..."
         # Looks like this doesn't work in VMC
         #$tagAssignments = @(Get-TagAssignment -ErrorAction Continue)
-        $tags = @(Get-Tag | Where-Object { $_.Name -match '^ci*|^qeci*' })
+        #$tags = @(Get-Tag | Where-Object { $_.Name -match '^ci*|^qeci*' })
+        $tags = @(Get-Tag)
         $folders = @(Get-Folder | Where-Object { $_.IsChildTypeVm -eq $true })
         $virtualMachines = @(Get-VM | Where-Object { $_.Name -match '^ci*|^qeci*' })
 
