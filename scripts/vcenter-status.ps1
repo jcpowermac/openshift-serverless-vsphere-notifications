023731a1ea2f96f4ca05b489bd5a1dd226655a9e
Joseph Callen <jcallen@redhat.com>
2023-07-06 15:24:33 -0400

    add vcenter status

---
+++ scripts/vcenter-status.ps1
@@ -0,0 +1,42 @@
+#!/bin/pwsh
+
+. /var/run/config/vcenter/variables.ps1
+
+Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
+
+$cihash = ConvertFrom-Json -InputObject $ci -AsHashtable
+
+$slackMessage = @"
+vCenter Status
+vcenter: {0}
+VM Count (soap): {1}
+Tag Count (rest): {2}
+"@
+
+foreach ($key in $cihash.Keys) {
+    $cihash[$key].vcenter
+    try {
+
+        Connect-VIServer -Server $cihash[$key].vcenter -Credential (Import-Clixml $cihash[$key].secret) | Out-Null
+
+        $vm = Get-VM
+        $tag = Get-Tag
+
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $cihash[$key].vcenter, $vm.Count, $tag.Count)
+
+    }
+    catch {
+        $caught = Get-Error
+        $errStr = $caught.ToString()
+
+        $caught
+
+        Send-SlackMessage -Color $_PSSlackColorMap.red -Uri $Env:SLACK_WEBHOOK_URI -Text $errStr
+        exit 1
+    }
+    finally {
+        Disconnect-VIServer -Server * -Force:$true -Confirm:$false
+    }
+}
+
+exit 0
