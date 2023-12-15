531641b95c19fd0c019cdffaff2cb81c69b31479
Joseph Callen <jcallen@redhat.com>
2023-12-15 08:54:59 -0500

    add ci lockout

---
+++ scripts/lockout.ps1
@@ -0,0 +1,47 @@
+#!/bin/pwsh
+. /var/run/config/vcenter/variables.ps1
+
+Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
+$cihash = ConvertFrom-Json -InputObject $ci -AsHashtable
+$slackMessage = @"
+*** WARNING ci account locked out ***
+vcenter: {0}
+account(s): {1}
+"@
+
+foreach ($key in $cihash.Keys) {
+
+    try {
+        $cihash[$key].vcenter
+
+        Connect-SsoAdminServer  -SkipCertificateCheck -Server $cihash[$key].vcenter -Credential (Import-Clixml $cihash[$key].secret) | Out-Null
+
+        $user,$domain = $DefaultSsoAdminServers.User -split '@'
+
+        $ssoUsers = Get-SsoPersonUser -Name "ci*" -Domain $domain
+
+        $lockedSsoAccounts = @()
+
+        foreach($user in $ssoUsers) {
+            if($user.Locked) {
+                $lockedSsoAccounts.Add($user.Name)
+            }
+
+        }
+
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $cihash[$key].vcenter, $lockedSsoAccounts -join " ")
+
+    }
+    catch {
+        $caught = Get-Error
+        $errStr = $caught.ToString()
+        $caught
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text $errStr
+    }
+    finally {
+        Disconnect-VIServer -Server * -Force:$true -Confirm:$false
+    }
+}
+
+
+exit 0
